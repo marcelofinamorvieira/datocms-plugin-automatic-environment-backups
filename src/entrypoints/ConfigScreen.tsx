1bcbd08f58a22afd7ae92312b4da5ce86324dda5
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2022-10-03 10:45:30 +0200

    Added navigation button

--- src/entrypoints/ConfigScreen.tsx
+++ src/entrypoints/ConfigScreen.tsx
@@ -10,10 +10,15 @@ export default function ConfigScreen({ ctx }: Props) {
     window.open(`https://app.netlify.com/`);
   };
 
+  const openBackupsPage = async () => {
+    await ctx.navigateTo("/admin/environments");
+  };
+
   return (
     <Canvas ctx={ctx}>
       <p>Weekly and daily backups have been successfully setup!</p>
       <Button onClick={openNetlifyPage}>Manage my functions</Button>
+      <Button onClick={openBackupsPage}>Manage my backups</Button>
     </Canvas>
   );
 }
